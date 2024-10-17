cef1406b014203e5176cd9a69fa222dec1c165f0
Robert Stolz <robert.stolz87@gmail.com>
2024-10-17 10:29:12 +0200

    Coding: Optional Product

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -27,6 +27,7 @@ public class ShopService {
             products.add(productToOrder);
         }
 
+
         Order newOrder = new Order(UUID.randomUUID().toString(), products);
         return orderRepo.addOrder(newOrder);
     }
